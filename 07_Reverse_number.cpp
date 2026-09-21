54dcf9527e1b574de57828d8f9580e6db9ebbca8
abhiighadge <abhijitghadge2009@gmail.com>
2026-09-21 23:00:13 +0530

    Added reverse number program

---
+++ 07_Reverse_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,reverse=0,remainder;
+    cout<<"Enter an integer:";
+    cin>>n;
+    while(n!=0)
+    {
+        remainder=n%10;
+        reverse=reverse*10+remainder;
+        n=n/10;
+
+    }
+    cout<<"Reversed Number="<<reverse<<endl;
+    return 0;
+}
